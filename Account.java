e4b71e08495b1882cca46e70268cd1613145a9f8
Dokjanii <139743973+Dokjanii@users.noreply.github.com>
2025-03-05 11:49:15 +0500

    Create Account.java

---
+++ Account.java
@@ -0,0 +1,31 @@
+package com.example.banking.model;
+
+public class Account {
+    private String username;
+    private double balance;
+
+    public Account(String username, double balance) {
+        this.username = username;
+        this.balance = balance;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public boolean withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
+}
